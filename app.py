89a664eba1d84f567ca23ae10cf2603a93a08f25
Bart Sosa-Teeuwen <78899124+bartteeuwen@users.noreply.github.com>
2026-08-02 23:18:59 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
+import os
+os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"
+
 import json
 import struct
 import pandas as pd
 import plotly.express as px
 import streamlit as st
 from google.cloud import storage
-from waymo_open_dataset.protos import scenario_pb2
-import os
-os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"
-
-# Now import your other libraries and scenario_pb2
-import streamlit as st
 from waymo_open_dataset.protos import scenario_pb2
 
 # --- Page configuration ---
